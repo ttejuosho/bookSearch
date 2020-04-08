e5267941b27305d233c35bf04581194d6f469263
Taiwo <ttejuosho@outlook.com>
2020-04-08 09:40:04 -0500

    press enter to search

--- javascript/app.js
+++ javascript/app.js
@@ -3,32 +3,39 @@ $(document).ready(function (){
     // Declare a variable for the words that are being searched
     var searchKeywords = [];
 
-    $(document).on("click", "#searchBook", function() {
+    $(document).keypress(function(e) {
+        var keycode = (e.keyCode ? e.keyCode : e.which);
+        if (keycode == '13' && $("#bookName").val().length > 0) {
+            getBookData();
+        }
+    });
 
+    $(document).on("click", "#searchBook", function() {
        if ( $("#bookName").val() === ""){
            alert("Please Enter A Book Name to Search")
        } else {
+            getBookData();
+       }
+    });
 
-        
+    function getBookData(){
+        event.preventDefault();
         var bookName = $("#bookName").val();
         $("#bookName").empty();
         $("#resultsDiv").empty();
         searchKeywords.push(bookName);
-        console.log(bookName);
-        console.log(searchKeywords);
-        
-
+    
         // Grabbing the text in the input field and storing in bookName Variable
             
             var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + bookName;
-
+    
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).done(function(response) {
-
+    
         //    console.log(response.items);
-
+    
                 for (var i = 0; i < 10; i++) {
                     // incase Book thumbnail isnt available, use BooGle Image
                     if (bookCover = undefined) {
@@ -55,21 +62,21 @@ $(document).ready(function (){
                     var publishedDate = response.items[i].volumeInfo.publishedDate;
                     var publisher = response.items[i].volumeInfo.publisher;
                     var bookCover = response.items[i].volumeInfo.imageLinks.thumbnail;
-
+    
                     // if other Book Info arent available say "Not Available"
                     // if (categories === undefined) { categories = "Not Available" } 
                     // else if ( pageCount === undefined){ pageCount = "Not Available" } 
                     // else if (description === undefined){description = "Not Available" } 
                     // else if (publisher === undefined){publisher = "Not Available" }
                     // else {publishedDate === "Not Available" }
-
-
+    
+    
         // Creating new Div and assigning to a newDiv variable and giving it a class of booksDiv   
                 var newDiv = $("<div>").addClass("booksDiv");
     
         // New ImageDiv with a Class of BookCover and assigning a src of the bookCover variable
                 var imageDiv = $("<img>").addClass("bookCover").attr("src", bookCover);
-
+    
         // new Div with class of bookInfo and insert all the bookinfo in the Div
                 var bookInfoDiv = $("<div>").addClass("bookInfo").html(
                         "Author: " + author + 
@@ -91,9 +98,7 @@ $(document).ready(function (){
                 
                
             });
-     } })
-
-
+     }
 
 });
 
